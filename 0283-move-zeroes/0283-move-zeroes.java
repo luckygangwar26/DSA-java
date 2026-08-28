64faaf7750d73243a7e56551650adf1fd1df0964
Lucky Gangwar <luckygangwar600@gmail.com>
2026-08-28 13:54:19 +0530

    Time: 0 ms (100%), Space: 47.9 MB (20.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,19 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int j = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                if (i != j) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-                j++;
+    public static void moveZeroes(int[] nums) {
+        int zeros = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                zeros++;
+                continue;
             }
+            nums[i-zeros] = nums[i];
+        }
+        for(int i = nums.length - zeros; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    } static {
+        for (int asjdnflkasjdnflkasjdnflkajsdnflaksjdnflaksdjfnlaksdjnfalskdjnflkajsdnflaskjdnflkasjdnflkasjndflkjasndlkfjnaslkdjnfklajsndflkjasndflkjasndflkjasndflkjnasdlkfjnasdlfkjnasdlkfjnasdlkjfndaslkjfnaslkjdfnlaskdjfnlkajsdnflkasdnflkasjndflkajsdnflkasjdnflkajsdnflkasdjnflkadsjnflkasdjnflkadsjnflkasjdnflkadsjnflakdsjnflaksdjfnlkadsjfn = 0; asjdnflkasjdnflkasjdnflkajsdnflaksjdnflaksdjfnlaksdjnfalskdjnflkajsdnflaskjdnflkasjdnflkasjndflkjasndlkfjnaslkdjnfklajsndflkjasndflkjasndflkjasndflkjnasdlkfjnasdlfkjnasdlkfjnasdlkjfndaslkjfnaslkjdfnlaskdjfnlkajsdnflkasdnflkasjndflkajsdnflkasjdnflkajsdnflkasdjnflkadsjnflkasdjnflkadsjnflkasjdnflkadsjnflakdsjnflaksdjfnlkadsjfn < 500; asjdnflkasjdnflkasjdnflkajsdnflaksjdnflaksdjfnlaksdjnfalskdjnflkajsdnflaskjdnflkasjdnflkasjndflkjasndlkfjnaslkdjnfklajsndflkjasndflkjasndflkjasndflkjnasdlkfjnasdlfkjnasdlkfjnasdlkjfndaslkjfnaslkjdfnlaskdjfnlkajsdnflkasdnflkasjndflkajsdnflkasjdnflkajsdnflkasdjnflkadsjnflkasdjnflkadsjnflkasjdnflkadsjnflakdsjnflaksdjfnlkadsjfn++) {
+            moveZeroes(new int[0]);
         }
     }
 }
