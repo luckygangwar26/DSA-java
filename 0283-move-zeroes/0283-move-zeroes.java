33d1811dd485a219176d01ae273b0c6969c3b222
Lucky Gangwar <luckygangwar600@gmail.com>
2026-08-28 13:53:04 +0530

    Time: 3 ms (17.97%), Space: 47.8 MB (57.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int j = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                for (int j = i+1; j < nums.length; j++) {
-                    if(nums[j]!=0){
+            if (nums[i] != 0) {
+                if (i != j) {
                     int temp = nums[i];
                     nums[i] = nums[j];
                     nums[j] = temp;
-                    break;
-                    }
                 }
+                j++;
             }
         }
-
     }
 }
