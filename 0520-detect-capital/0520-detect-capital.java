192a565eaee37909b731bd59d917be9804923fc7
Lucky Gangwar <luckygangwar600@gmail.com>
2026-08-30 21:46:58 +0530

    Time: 1 ms (89.53%), Space: 43.1 MB (78.05%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                count++;
+            }
+
+        }
+        if (count == 0) {
+            return true;
+        } else if (count == 1 && Character.isUpperCase(word.charAt(0))) {
+            return true;
+        } else if (count == word.length()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
