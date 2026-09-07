bde1921bc4eff28e466409429ee794a295739c5f
Lucky Gangwar <luckygangwar600@gmail.com>
2026-09-07 12:11:19 +0530

    Time: 191 ms (5.34%), Space: 47 MB (5.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        while (s.contains("()") ||
+                s.contains("{}") ||
+                s.contains("[]")) {
+
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            }
+
+            if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            }
+
+            if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            }
+        }
+
+        if (s.length() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
