35e8a57e12cf218e2f4937309ac7b92057163dc6
Lucky Gangwar <luckygangwar600@gmail.com>
2026-09-08 15:35:50 +0530

    Time: 3 ms (64.56%), Space: 46.9 MB (21.66%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    private boolean check(char ch) {
+        return "aeiouAEIOU".indexOf(ch) != -1;
+    }
+
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            if (!check(arr[i])) {
+                i++;
+            } else if (!check(arr[j])) {
+                j--;
+            } else {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        return new String(arr);
+    }
+}
