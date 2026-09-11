76bc5c7b7b8463507530cc185945c7e2d1c3e6fb
Lucky Gangwar <luckygangwar600@gmail.com>
2026-09-12 00:16:58 +0530

    Time: 0 ms (100%), Space: 43.1 MB (39.43%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,18 +1,35 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-       String s1 = removeBackSpace(s);
-       String s2 = removeBackSpace(t) ;
-       return s1.equals(s2);
-    }
-String removeBackSpace(String s1){
-    StringBuilder sb = new StringBuilder();
-    for(char ch : s1.toCharArray()){
-        if(ch != '#'){
-            sb.append(ch);
-        }else if(sb.length() > 0 && ch == '#'){
-            sb.deleteCharAt(sb.length()-1);
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        while (i >= 0 || j >= 0) {
+            i = getNextValidIndex(s, i);
+            j = getNextValidIndex(t, j);
+
+            if (i < 0 && j < 0) return true;
+            if (i < 0 || j < 0) return false;
+            if (s.charAt(i) != t.charAt(j)) return false;
+
+            i--;
+            j--;
         }
+        return true;
     }
-    return sb.toString();
-}
+
+    private int getNextValidIndex(String str, int index) {
+        int skip = 0;
+        while (index >= 0) {
+            if (str.charAt(index) == '#') {
+                skip++;
+                index--;
+            } else if (skip > 0) {
+                skip--;
+                index--;
+            } else {
+                break;
+            }
+        }
+        return index;
+    }
 }
